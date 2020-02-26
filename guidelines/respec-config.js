4782fdcdc9638edd18aa2212560b095ee6fccd80
Michael Cooper <cooper@w3.org>
2020-02-26 10:42:05 -0500

    restage for 27 Feb

--- guidelines/respec-config.js
+++ guidelines/respec-config.js
@@ -11,7 +11,7 @@ var respecConfig = {
 	specStatus:           "FPWD",
 	//crEnd:                "2012-04-30",
 	//perEnd:               "2013-07-23",
-	publishDate:          "2020-02-25",
+	publishDate:          "2020-02-27",
 	diffTool:             "http://www.aptest.com/standards/htmldiff/htmldiff.pl",
 	
 	// the specifications short name, as in https://www.w3.org/TR/short-name/
