f02eb4deb1b57891479ef97570efc4c849525ed5
Michael Cooper <cooper@w3.org>
2020-04-30 16:10:51 -0400

    restage for 1 may

--- conformance-challenges/respec-config.js
+++ conformance-challenges/respec-config.js
@@ -19,7 +19,7 @@ var respecConfig = {
 	
 	
 	// if you wish the publication date to be other than today, set this
-	publishDate:  "2020-04-30",
+	publishDate:  "2020-05-01",
 	copyrightStart:  "2020",
 	license: "w3c-software-doc",
 	noRecTrack: true,
